80b227169da2d5009836d4ced4cbb1653f5d48c5
dancielos <iam@dancielos.com>
2023-12-26 22:19:36 -0700

    added webpack config

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ProjectInput_1 = require("./Components/ProjectInput");
-const ProjectList_1 = require("./Components/ProjectList");
-new ProjectInput_1.ProjectInput();
-new ProjectList_1.ProjectList('active');
-new ProjectList_1.ProjectList('finished');
-//# sourceMappingURL=app.js.map
